db8df069f046f81b7e0ba5b07ae44c9089373612
Clansty <i@gao4.pw>
2025-11-21 06:03:11 +0800

    bump version

--- MaiChartManager/AppMain.cs
+++ MaiChartManager/AppMain.cs
@@ -12,7 +12,7 @@ namespace MaiChartManager;
 
 public class AppMain : ISingleInstance
 {
-    public const string Version = "1.7.1";
+    public const string Version = "1.7.2";
     public static Browser? BrowserWin { get; set; }
 
     private Launcher _launcher;
